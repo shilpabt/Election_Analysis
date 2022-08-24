9dbe0e2d9b2bc79a183297c652dce3bbe8d730d4
Shilpa Belagutti <shilpabtsmg@gmail.com>
2022-08-24 12:57:32 -0500

    Adding the Election audit code.

---
+++ List_practice.py
@@ -0,0 +1,12 @@
+grocery_items=["Milk","Bread", "Eggs","Peanut Butter","Jelly"]
+print(grocery_items)
+grocery_items.insert(3, "almond butter")
+grocery_items.remove("Jelly")
+print(grocery_items)
+grocery_items.remove("Peanut Butter")
+print(grocery_items)
+grocery_items.append("coffee")
+
+print(grocery_items)
+
+
---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+
+name ="John"
+country= "US"
+age = 30
+hourly_wage = 100
+satisfied = True
+daily_wage = hourly_wage * 8
+
+print("Name: "+ name, " Age: "+ str(age)+ " country: " + country+ "hourly_wage: "+str(hourly_wage) )
+print(f"My daily wage : {daily_wage}")
+print(f"It's  {satisfied}")
+
